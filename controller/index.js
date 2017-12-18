f4f294d328a1be37de1b0d2e85c48286f50a59db
stkraft <steffen.kraft@mail.de>
2017-12-18 12:31:05 +0100

    adding Store, createStore controller, route add store, slug

--- controller/index.js
+++ controller/index.js
@@ -1,3 +1,16 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
     res.json({ name: "hans", cool: true });
 }
+
+exports.createStore = async (req, res) => {
+    const store = new Store({
+        name: 'shop Name',
+        description: 'description sfsda fdsa g dasg sad g',
+        tags: ['shoes', 'shorts', 'underwear']
+    });
+    await store.save();
+    res.redirect('/');
+};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controller');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', controller.homePage);
+
+router.get('/add', catchErrors(controller.createStore));
 
 module.exports = router;
