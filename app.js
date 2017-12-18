3fe49e710061ef2713940ea9bff3b739a1d93441
stkraft <steffen.kraft@mail.de>
2017-12-18 10:13:59 +0100

    adding error handler middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
+const errorHandlers = require('./handlers/errorHandlers')
 
 // create our Express app
 const app = express();
@@ -11,6 +12,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // After allllll that above middleware, we finally handle our own routes!
 app.use('/', routes);
+
+// If that above routes didnt work, we 404 them and forward to error handler
+app.use(errorHandlers.notFound);
+
+// Otherwise this was a really bad error we didn't expect! Shoot eh
+if (app.get('env') === 'development') {
+  /* Development Error Handler - Prints stack trace */
+  app.use(errorHandlers.developmentErrors);
+}
 
 // done! we export it so we can start the site in start.js
 module.exports = app;
